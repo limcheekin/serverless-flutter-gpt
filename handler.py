5ce0415dd47bf57df6caf39b0dbf1ff1d6fd960e
Lim Chee Kin <limcheekin@vobject.com>
2023-03-28 12:08:59 +0800

    feat: added webhook endpoint for channels

--- handler.py
+++ handler.py
@@ -1,5 +1,7 @@
 import json
 import pickle
+import base64
+import urllib.parse
 
 # REF: https://www.kaggle.com/prmohanty/python-how-to-save-and-load-ml-models
 model_name = 'faiss_store.pkl'
@@ -33,7 +35,34 @@ def ask_question(event, context):
 
     return response
 
-def do_main():
+def webhook(event, context):
+    body = event.get("body")
+
+    if event.get("isBase64Encoded"): 
+        base64_bytes = base64.b64decode(body)
+        body = base64_bytes.decode("utf-8")
+    
+    body = urllib.parse.parse_qs(body)
+    print(f"body:\n{body}")
+    context = json.loads(body['contextobj'][0])
+    sender = json.loads(body['senderobj'][0])
+    message = json.loads(body['messageobj'][0])
+
+    response = f"botname: {context['botname']}, channel: {context['channeltype']}, traceId: {context['traceId']}, contextid: {context['contextid']}, "
+    response += f"sender: {sender['display']}, "
+    response += f"message id: {message['id']}, type: {message['type']}, text: {message['text']}, timestamp: {message['timestamp']}"
+
+
+    print(response)
+    return {
+        "statusCode": 200,
+        "body": response,
+        "headers": {
+            "Content-Type": 'text/plain',
+        }
+    }   
+
+def test_ask_question():
     event = {
         'message': {
             'text': "What is the latest version of Flutter?"
@@ -45,4 +74,10 @@ def do_main():
     with open('event.json', 'w') as event_file:
         event_file.write(json.dumps(event))
         
-#do_main()
+def test_webhook():
+    with open('webhook_event.json') as json_file:
+        file_contents = json_file.read()
+    event = json.loads(file_contents)
+    response = webhook(event, None)
+
+test_webhook()
